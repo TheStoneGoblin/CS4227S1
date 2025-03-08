648802f2d0157889c51c848ea1b71a072c580bf9
Gabriel <123stanadrian@gmail.com>
2025-03-08 21:03:28 +0000

    made changes for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/your-repo-name/',
+  base: '/CS4227S1/',
 })
